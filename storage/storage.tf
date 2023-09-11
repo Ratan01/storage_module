6d7f46b4d1213c8f4581d372784c2405ff5f6c8f
Ratan01 <ratanrudra010@gmail.com>
2023-09-11 12:09:35 +0530

    storage file updated

--- storage/storage.tf
+++ storage/storage.tf
@@ -2,15 +2,15 @@ provider "azurerm"{
     features {
     }
 }
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "rg" {
+  name     = "storage-resources"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "st" {
+  name                     = "storageaccountrk12"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
